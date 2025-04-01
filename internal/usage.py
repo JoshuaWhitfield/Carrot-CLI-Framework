3e3b0a5a20b795afe9a8ef87e1b5563b486df1a2
Joshua Whitfield <joshuawhitfield022@gmail.com>
2025-04-01 01:58:33 -0700

    imported basic cli

---
+++ internal/usage.py
@@ -0,0 +1,33 @@
+from internal.usage_object import UsageObject
+
+class Usage:
+    def __init__(self):
+        self.body = {}
+
+    def add_usage(self, cmd_name, usage_array, description, long, short):
+        usage_object = UsageObject(cmd_name, usage_array, description, long, short)
+        self.body[cmd_name] = usage_object
+        return usage_object
+    
+    def get_usage(self, cmd_name):
+        if cmd_name not in self.body:
+            return None
+        return self.body[cmd_name]
+    
+    def display(self, usage_object):
+        # Fixing the join() issue for usage_array
+        print("COMMAND: " + usage_object.get_name().upper())
+        print("  " + "\n ".join(usage_object.get_usage_array()))  # Correct usage of join
+        print(" desc: " + usage_object.get_description())
+        print(" long: " + usage_object.get_long())
+        if usage_object.get_short() != "":
+            print(" short: " + usage_object.get_short())
+        return True
+
+# Example usage
+usage = Usage()
+
+usage.add_usage("clear", [" clear ", " cls "], "this command clears the terminal.", " clear ", " cls ")
+usage.add_usage("data", [" data collect['<string_keyword>', ...] ", " data collect['<string_keyword>', ...] -parse['/full/path/to/parser']", "data -collect['<keywords>'] -parse[]"], "this command can collect and parse information based on a template. by ommitting the path from the -parse flag, you use the built in parser.", " data ", "")
+usage.add_usage("db", [" db -show <table_name> "], "this command displays the contents stored in the database powered by Memurai.", "db", "")
+# Test displaying usage
---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from syntax.lexer import Lexer 
+from syntax.parser import Parser 
+from syntax.interpreter import Interpreter
+from syntax.prompt import Prompt 
+
+lexer = Lexer()
+parser = Parser()
+interp = Interpreter()
+prompt = Prompt()
+
+while True:
+    inputs = prompt.get_input().split(";")
+
+    for input in inputs:
+        
+        lexer.set_input(lexer.reset_output() + list(input)) # reassign token and flush output token array
+        lexer.Tokenize()
+        
+        parser.set_input(parser.reset_output() + lexer.get_output())
+        parser.parse()
+
+        interp.set_input(interp.reset_input() + parser.get_output())
+        interp.Execute()
+    
+
+    
+
+
